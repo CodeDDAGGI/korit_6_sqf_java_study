8dd1afa2a7f4f9e4fb334b2c624f7165093d014d
bg kim <154308455+CodeDDAGGI@users.noreply.github.com>
2024-05-28 00:03:07 +0900

    Update BookArrayService02.java

--- src/main/java/com/study/java_study/ch06_배열/BookArrayService02.java
+++ src/main/java/com/study/java_study/ch06_배열/BookArrayService02.java
@@ -7,8 +7,93 @@ public class BookArrayService02 {
 
         Scanner scanner = new Scanner(System.in);
         String[] books = new String[0];
-        boolean Run = true;
-        BookArrayUtils Arraybooks = new BookArrayUtils();
+        boolean isRun = true;
+        BookArrayUtils bookArrayUtils = new BookArrayUtils();
 
+        while (isRun) {
+            System.out.println("[도서 관리 프로그램]");
+            System.out.println("1. 등록");
+            System.out.println("2. 수정");
+            System.out.println("3. 삭제");
+            System.out.println("4. 찾기");
+            System.out.println("5. 전체 조회");
+            System.out.println("q. 종료");
+            System.out.print("메뉴 선택 : ");
+            String menuSeleter = scanner.nextLine();
+            int findIndex = -1;
+            String[] newBooks = new String[books.length + 1];
+
+            switch (menuSeleter) {
+                case "1":
+                    System.out.println("등록");
+                    for (int i = 0; i < books.length; i++) {
+                        newBooks[i] = books[i];
+                    }
+                    books = newBooks;
+                    System.out.print("책 이름 : ");
+                    books[books.length - 1] = scanner.nextLine();
+                    break;
+
+                case "2":
+                    System.out.println("수정");
+                    System.out.print("수정할 이름 : ");
+                    String modifybook = scanner.nextLine();
+                    findIndex = bookArrayUtils.bookFindByBooks(books, modifybook);
+                    if (findIndex == -1) {
+                        System.out.println("책을 찾지 못했습니다");
+                        break;
+                    }
+                    System.out.println("새로 등록할 이름 : ");
+                    books[findIndex] = scanner.nextLine();
+                    System.out.println("완료");
+                    break;
+                case "3":
+                    System.out.println("삭제");
+                    System.out.println("삭제할 이름 : ");
+                    String deleteBook = scanner.nextLine();
+                    findIndex = bookArrayUtils.bookFindByBooks(books, deleteBook);
+                    if (findIndex == -1) {
+                        System.out.println("책을 찾지 못했습니다");
+                        break;
+                    }
+                    newBooks = new String[books.length - 1];
+                    for (int i = 0; i < newBooks.length; i++) {
+                        if (i < findIndex) {
+                            newBooks[i] = books[i];
+                            continue;
+                        }
+                        newBooks[i] = books[i + 1];
+                    }
+                    books = newBooks;
+                    System.out.println("삭제 완료");
+                    break;
+
+                case "4":
+                    System.out.println("찾기");
+                    System.out.println("책 이름 : ");
+                    String findbook = scanner.nextLine();
+                    findIndex = bookArrayUtils.bookFindByBooks(books, findbook);
+                    for (int i = 0; i < books.length; i++) {
+                        if (findIndex == -1) {
+                            System.out.println("책을 찾지 못헀습니다");
+                            break;
+                        }
+                    }
+                    System.out.println("책 번호 " + (findIndex + 1) + "책 이름 : " + books[findIndex]);
+                    break;
+                case "5":
+                    for (int i = 0; i < books.length; i++) {
+                        System.out.println("책" + (i + 1) + " 책 이름 : " + books[i]);
+                    }
+                    break;
+                case "q":
+                    System.out.println("프로그램 종료");
+                    isRun = false;
+                    break;
+                default:
+                    System.out.println("다시 입력해주세요.");
+
+            }
+        }
     }
 }
